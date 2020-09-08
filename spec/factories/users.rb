f59fe0e02ef3c460d30bf79a571f9dfd32a3a68f
yunosuke0522 <yunosuke.otake@di-v.co.jp>
2020-09-08 21:32:53 +0900

    Revert "a"
    This reverts commit 4ad8db1b1265d31a05a333184c20a446d71cf124.

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 8)
-    password {password}
-    password_confirmation {password}
-  end
-end
